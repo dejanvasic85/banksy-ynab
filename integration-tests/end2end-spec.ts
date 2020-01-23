17654f15496113d7060e0b36e697480551ab2710
Dejan Vasic <dvasic@seek.com.au>
2020-01-23 16:51:02 +1100

    Adding some end to end tests

---
+++ integration-tests/end2end-spec.ts
@@ -0,0 +1,41 @@
+import * as AWS from 'aws-sdk';
+AWS.config.update({
+  accessKeyId: 'AKIAQJNYCGAACCUOMQ4F',
+  secretAccessKey: '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',
+  region: 'ap-southeast-2',
+});
+
+const snsTopicArn = 'arn:aws:sns:ap-southeast-2:020250243072:banksy-transactions-dev';
+const snsClient = new AWS.SNS();
+
+describe('end 2 end test', () => {
+  const now = new Date().toISOString();
+  const testTransactions = [
+    {
+      date: now,
+      amount: -5,
+      description: `Test Transaction ${now}`,
+    },
+  ];
+
+  const snsMessage = {
+    username: 'spec',
+    accountName: 'Complete Freedom Offset',
+    transactions: testTransactions,
+  };
+
+  before(async () => {
+    const result = await snsClient
+      .publish({
+        Message: JSON.stringify(snsMessage),
+        TopicArn: snsTopicArn,
+      })
+      .promise();
+
+    console.log('result', result.MessageId);
+  });
+
+  it('should publish a new transaction to YNAB', () => {
+    console.log('todo - poll and check ynab');
+  });
+});
