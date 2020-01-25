50fb288723a7c424b2c3303f761a21fe5cf4b5a0
Dejan Vasic <dejanvasic24@gmail.com>
2020-01-25 16:28:06 +1100

    Getting the integration test working

--- integration-tests/end2end-spec.ts
+++ integration-tests/end2end-spec.ts
@@ -1,19 +1,47 @@
 import * as AWS from 'aws-sdk';
+import { Budget } from '../src/budget';
 
 AWS.config.update({
   region: 'ap-southeast-2',
 });
 
-const snsTopicArn = 'arn:aws:sns:ap-southeast-2:020250243072:banksy-transactions-dev';
 const snsClient = new AWS.SNS();
+const { YNAB_KEY, YNAB_BUDGET_ID, YNAB_ACCOUNT_ID } = process.env;
+
+const config = {
+  snsTopicArn: 'arn:aws:sns:ap-southeast-2:020250243072:banksy-transactions-dev',
+  ynabKey: YNAB_KEY,
+  ynabBudgetId: YNAB_BUDGET_ID,
+  ynabAccountId: YNAB_ACCOUNT_ID,
+};
+
+const poll = async (fn: any, timeout: number, interval: number) => {
+  const endTime = Date.now() + timeout;
+
+  const checkCondition = async (resolve: any, reject: any) => {
+    const result = await fn();
+    if (result) {
+      resolve(result);
+    } else if (Date.now() < endTime) {
+      setTimeout(checkCondition, interval, resolve, reject);
+    } else {
+      reject(new Error(`Timed out during polling.`));
+    }
+  };
+
+  return new Promise(checkCondition);
+};
 
 describe('end 2 end test', () => {
+  let budget: Budget;
+
   const now = new Date().toISOString();
+  const description = `Test Transaction ${now}`;
   const testTransactions = [
     {
       date: now,
-      amount: -5,
-      description: `Test Transaction ${now}`,
+      amount: -1,
+      description,
     },
   ];
 
@@ -25,17 +53,32 @@ describe('end 2 end test', () => {
   };
 
   before(async () => {
-    const result = await snsClient
+    await snsClient
       .publish({
         Message: JSON.stringify(snsMessage),
-        TopicArn: snsTopicArn,
+        TopicArn: config.snsTopicArn,
       })
       .promise();
 
-    console.log('result', result.MessageId);
+    budget = new Budget(config.ynabKey);
+    await budget.loadBudget(config.ynabBudgetId);
   });
 
-  it('should publish a new transaction to YNAB', () => {
-    console.log('todo - poll and check ynab');
+  it('should publish a new transaction to YNAB', async () => {
+    await poll(
+      async () => {
+        // Fetch the ynab transactions
+        console.log('Fetching transactions please wait...');
+
+        const days = 1;
+        const txns = await budget.getTransactionsForAccount(config.ynabAccountId, days);
+        const foundTransaction = txns.find(({ amount, memo }) => amount === -1000 && memo === description);
+
+        console.log('Found ', foundTransaction);
+        return foundTransaction;
+      },
+      20000, // up to 20 seconds
+      1000, // every second
+    );
   });
 });
