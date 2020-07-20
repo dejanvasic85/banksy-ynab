236ddc140eba6b9cd4aae6992c0c7d6f07c143b3
Dejan Vasic <dejanvasic24@gmail.com>
2020-07-21 00:30:12 +1000

    fix: Floor on the amount to prevent from math overflow numbers

--- src/money.ts
+++ src/money.ts
@@ -1,7 +1,7 @@
 const AUD_BASE = 1000;
 
 export const toBaseValue = (value: number): number => {
-  return value * AUD_BASE;
+  return Math.floor(value * AUD_BASE);
 };
 
 export const fromBaseValue = (value: number): number => {
